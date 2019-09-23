07208c70ef73a52b9a9593dca4064c5cfa3732a4
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-09-23 07:53:23 -0700

    removed samples

--- src/main/scala/com/xyzcorp/demos/mocking/Student.scala
+++
@@ -1,9 +0,0 @@
-package com.xyzcorp.demos.mocking
-
-case class Student(id:Option[Long], ssn:String,
-                   firstName:String, lastName:String)
-
-object Student {
-    def apply(ssn:String, firstName:String, lastName:String) =
-        new Student(None, ssn, firstName, lastName)
-}
